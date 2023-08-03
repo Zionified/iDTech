1aeea8075c52b072728988ba115afa7a703a487d
Zionified <vickyzixuanzheng@gmail.com>
2023-08-03 15:46:16 -0700

    added codes to detectnet

---
+++ detectnet/detectnet.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+
+import sys
+import argparse
+
+from jetson_inference import detectNet
+from jetson_utils import videoSource, videoOutput, Log
+from process_image import process_image
+
+# parse the command line
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, 
+                                 epilog=detectNet.Usage() + videoSource.Usage() + videoOutput.Usage() + Log.Usage())
+
+parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
+parser.add_argument("--overlay", type=str, default="label,box,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
+
+try:
+	args = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+# process the image using default detectnet
+default_objects = process_image(args.input, args.output, args.network, args.threshold, args.overlay, DEFAULT=True)
+print(default_objects)
+
+# if person is detected, immediately give warning
+if "person" in default_objects.keys():
+    print("Warning!")
+else:
+    # otherwise, check if fox or bear is present using the trained model
+    fox_objects = process_image(args.input, args.output, args.network, args.threshold, args.overlay, DEFAULT=False)
+
+    # if fox or bear is detected and no people are detected, give safe flag
+    if fox_objects or "Bear" in default_objects.keys():
+        print("Safe!")
+    else:
+        print("Nothing is detected!")
+
+    print(fox_objects)
+    print(default_objects)
